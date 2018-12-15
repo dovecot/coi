e4f5b15ee50bd9b18e37f2e73bcd4318e1b5db69
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-12-15 10:33:33 +0100

    Create lib-coi and use it in all plugins.

---
+++ src/lib-coi/coi-common.h
@@ -0,0 +1,20 @@
+#ifndef COI_COMMON_H
+#define COI_COMMON_H
+
+/*
+ * Message headers
+ */
+
+#define COI_MSGHDR_CHAT "X-COI-Chat"
+#define COI_MSGHDR_TEMP_TOKEN "X-COI-Temporary-Token"
+
+/*
+ * Mailboxes
+ */
+
+#define COI_MAILBOX_DEFAULT_ROOT "COI"
+
+#define COI_MAILBOX_CHATS "Chats"
+#define COI_MAILBOX_CONTACTS "Contacts"
+
+#endif
