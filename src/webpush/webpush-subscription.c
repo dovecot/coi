dovecot/coi

commit 39da2df1a3d8d859e7673f0da258ff1b6c8d5e95

webpush: Add support for subscription registration
The actual push-notification sending will be done in a separate commit
later.

Added a new setting webpush_subscription_expire=<interval> to specify how
long a subscription registration is valid before it needs to be finished.

src/webpush/webpush-subscription.c
@@ -1,0 +1,420 @@
+/* Copyright (c) 2019 Dovecot authors, see the included COPYING file */
+
+#include "lib.h"
+#include "ioloop.h"
+#include "randgen.h"
+#include "base64.h"
+#include "str.h"
+#include "istream.h"
+#include "iso8601-date.h"
+#include "json-parser.h"
+#include "settings-parser.h"
+#include "mail-storage-private.h"
+#include "webpush-subscription.h"
+
+#define WEBPUSH_DEFAULT_SUBSCRIPTION_EXPIRE_SECS (5*60)
+
+#define WEBPUSH_DEVICE_KEY_MAX_LENGTH 256
+
+static bool
+webpush_subscription_is_expired(struct mailbox *box,
+				const struct webpush_subscription *subscription)
+{
+	const char *str, *error;
+	unsigned int expire_secs = WEBPUSH_DEFAULT_SUBSCRIPTION_EXPIRE_SECS;
+
+	if (subscription->validation == NULL) {
+		/* already validated */
+		return FALSE;
+	}
+
+	if (ioloop_time < subscription->create_time) {
+		/* time moved backwards - allow anyway.. */
+		return FALSE;
+	}
+	str = mail_user_plugin_getenv(box->storage->user, "webpush_subscription_expire");
+	if (str != NULL && settings_get_time(str, &expire_secs, &error) < 0)
+		i_error("webpush: Invalid webpush_subscription_expire=%s: %s - ignoring", str, error);
+	return ioloop_time - subscription->create_time >= expire_secs;
+}
+
+static bool
+webpush_subscription_validate_resource_keys(const struct webpush_subscription *subscription,
+					    const char **error_r)
+{
+	const struct webpush_resource_key *key;
+	bool have_p256dh = FALSE, have_auth = FALSE;
+
+	array_foreach(&subscription->resource_keys, key) {
+		if (strcmp(key->key, "p256dh") == 0)
+			have_p256dh = TRUE;
+		else if (strcmp(key->key, "auth") == 0)
+			have_auth = TRUE;
+		else {
+			*error_r = t_strdup_printf("Unknown resource key '%s'", key->key);
+			return FALSE;
+		}
+	}
+	if (!have_p256dh) {
+		*error_r = "resource { key { p256dh } } is missing";
+		return FALSE;
+	}
+	if (!have_auth) {
+		*error_r = "resource { key { auth } } is missing";
+		return FALSE;
+	}
+	return TRUE;
+}
+
+static bool
+webpush_subscription_validate(const struct webpush_subscription *subscription,
+			      const char **error_r)
+{
+	const char *missing = NULL;
+
+	/* make sure everything exists */
+	if (subscription->client_name == NULL)
+		missing = "client";
+	else if (subscription->device_name == NULL)
+		missing = "device";
+	else if (subscription->msgtype == WEBPUSH_SUBSCRIPTION_MSGTYPE_UNKNOWN)
+		missing = "msgtype";
+	else if (subscription->resource_endpoint == NULL)
+		missing = "resource { endpoint }";
+	else if (!array_is_created(&subscription->resource_keys))
+		missing = "resource { keys }";
+
+	if (missing != NULL) {
+		*error_r = t_strdup_printf("%s is missing", missing);
+		return FALSE;
+	}
+	if (!webpush_subscription_validate_resource_keys(subscription, error_r))
+		return FALSE;
+	return TRUE;
+}
+
+static void webpush_json_append_comma_if_needed(string_t *str)
+{
+	i_assert(str_len(str) > 0);
+	switch (str_data(str)[str_len(str)-1]) {
+	case '{':
+	case ',':
+		break;
+	default:
+		str_append_c(str, ',');
+		break;
+	}
+}
+
+static void
+webpush_append_keyvalue(string_t *str, const char *key, const char *value)
+{
+	if (value == NULL)
+		return;
+
+	webpush_json_append_comma_if_needed(str);
+	str_append_c(str, '"');
+	json_append_escaped(str, key);
+	str_append(str, "\":\"");
+	json_append_escaped(str, value);
+	str_append_c(str, '"');
+}
+
+static const char *
+webpush_subscription_to_string(const struct webpush_subscription *subscription,
+			       bool internal)
+{
+	string_t *str = t_str_new(128);
+
+	str_append_c(str, '{');
+	str_printfa(str, "\"create_time\":\"%s\"",
+		    iso8601_date_create(subscription->create_time));
+	if (internal) {
+		webpush_append_keyvalue(str, "validation",
+					subscription->validation);
+	} else {
+		str_printfa(str, ",\"validated\":%s",
+			    subscription->validation == NULL ? "true" : "false");
+	}
+	webpush_append_keyvalue(str, "client", subscription->client_name);
+	webpush_append_keyvalue(str, "device", subscription->device_name);
+	if (subscription->msgtype != WEBPUSH_SUBSCRIPTION_MSGTYPE_UNKNOWN) {
+		webpush_append_keyvalue(str, "msgtype",
+			webpush_subscription_msgtype_to_string(subscription->msgtype));
+	}
+
+	webpush_json_append_comma_if_needed(str);
+	str_append(str, "\"resource\":{");
+	webpush_append_keyvalue(str, "endpoint", subscription->resource_endpoint);
+	if (array_is_created(&subscription->resource_keys) > 0) {
+		const struct webpush_resource_key *key;
+
+		webpush_json_append_comma_if_needed(str);
+		str_append(str, "\"keys\":{");
+		array_foreach(&subscription->resource_keys, key)
+			webpush_append_keyvalue(str, key->key, key->value);
+		str_append_c(str, '}');
+	}
+	str_append(str, "}}");
+	return str_c(str);
+}
+
+static int
+webpush_subscription_parse_value(const struct mail_attribute_value *value,
+				 pool_t pool,
+				 struct webpush_subscription *subscription_r,
+				 const char **error_r)
+{
+	int ret;
+
+	if (value->value_stream != NULL) {
+		ret = webpush_subscription_parse(value->value_stream, pool,
+						 subscription_r, error_r);
+	} else {
+		struct istream *input =
+			i_stream_create_from_data(value->value, strlen(value->value));
+		ret = webpush_subscription_parse(input, pool,
+						 subscription_r, error_r);
+		i_stream_unref(&input);
+	}
+	if (ret < 0) {
+		/* make sure we don't read partial subscription */
+		i_zero(subscription_r);
+	}
+	return ret;
+}
+
+static void
+webpush_subscription_delete(struct mailbox *box, const char *storage_key)
+{
+	struct mailbox_transaction_context *t;
+
+	if (mailbox_open(box) < 0) {
+		i_error("webpush: Failed to unset delete subscription %s: "
+			"Mailbox couldn't be opened: %s",
+			storage_key, mailbox_get_last_internal_error(box, NULL));
+		return;
+	}
+
+	t = mailbox_transaction_begin(box, 0, "webpush subscription delete");
+	mailbox_attribute_unset(t, MAIL_ATTRIBUTE_TYPE_PRIVATE, storage_key);
+	if (mailbox_transaction_commit(&t) < 0) {
+		i_error("webpush: Failed to unset delete subscription %s: %s",
+			storage_key, mailbox_get_last_internal_error(box, NULL));
+	}
+}
+
+int webpush_subscription_read(struct mailbox *box, const char *device_key,
+			      pool_t pool,
+			      struct webpush_subscription *subscription_r)
+{
+	struct mail_attribute_value value;
+	const char *storage_key, *error;
+	int ret;
+
+	storage_key = t_strconcat(MAILBOX_ATTRIBUTE_WEBPUSH_PRIVATE_SUBSCRIPTION_PREFIX,
+				  device_key, NULL);
+
+	i_zero(subscription_r);
+	ret = mailbox_attribute_get_stream(box, MAIL_ATTRIBUTE_TYPE_PRIVATE,
+					   storage_key, &value);
+	if (ret <= 0)
+		return ret;
+
+	if (webpush_subscription_parse_value(&value, pool, subscription_r, &error) < 0 ||
+	    !webpush_subscription_validate(subscription_r, &error)) {
+		i_error("webpush: Invalid subscription %s - deleting: %s",
+			device_key, error);
+		webpush_subscription_delete(box, storage_key);
+		return 0;
+	}
+	if (webpush_subscription_is_expired(box, subscription_r)) {
+		/* already expired - delete silently */
+		webpush_subscription_delete(box, storage_key);
+		return 0;
+	}
+	return 1;
+}
+
+static int
+webpush_subscription_attribute_get(struct mailbox *box, const char *key,
+				   struct mail_attribute_value *value_r)
+{
+	struct webpush_subscription subscription;
+	const char *device_key;
+	int ret;
+
+	i_assert(str_begins(key, MAILBOX_ATTRIBUTE_WEBPUSH_PUBLIC_SUBSCRIPTION_PREFIX));
+	device_key = key + strlen(MAILBOX_ATTRIBUTE_WEBPUSH_PUBLIC_SUBSCRIPTION_PREFIX);
+
+	ret = webpush_subscription_read(box, device_key,
+					pool_datastack_create(), &subscription);
+	if (ret <= 0)
+		return ret;
+
+	value_r->value = webpush_subscription_to_string(&subscription, FALSE);
+	return 1;
+}
+
+static int
+webpush_subscription_store(struct mailbox_transaction_context *t,
+			   const char *device_key,
+			   const struct webpush_subscription *subscription)
+{
+	struct mail_attribute_value storage_value;
+	const char *storage_key;
+
+	storage_key = t_strconcat(MAILBOX_ATTRIBUTE_WEBPUSH_PRIVATE_SUBSCRIPTION_PREFIX,
+				  device_key, NULL);
+	i_zero(&storage_value);
+	storage_value.value = webpush_subscription_to_string(subscription, TRUE);
+	if (mailbox_attribute_set(t, MAIL_ATTRIBUTE_TYPE_PRIVATE,
+				  storage_key, &storage_value) < 0) {
+		i_error("webpush: Can't store subscription: %s", 
+			mail_storage_get_last_internal_error(t->box->storage, NULL));
+		return -1;
+	}
+	return 0;
+}
+
+static int
+webpush_subscription_validation_set(struct mailbox_transaction_context *t,
+				    const char *device_key,
+				    const struct mail_attribute_value *value)
+{
+	struct webpush_subscription old_subscription;
+	const char *validation;
+	int ret;
+
+	if (mailbox_attribute_value_to_string(t->box->storage, value, &validation) < 0)
+		return -1;
+	if (validation == NULL) {
+		mail_storage_set_error(t->box->storage, MAIL_ERROR_PARAMS,
+				       "Can't unset validation key");
+		return -1;
+	}
+
+	/* Finishing the registration. Verify that the given validation string
+	   is the same one that is currently stored. */
+	ret = webpush_subscription_read(t->box, device_key,
+					pool_datastack_create(),
+					&old_subscription);
+	if (ret < 0)
+		return -1;
+
+	if (ret == 0 || old_subscription.validation == NULL ||
+	    strcmp(old_subscription.validation, validation) != 0) {
+		mail_storage_set_error(t->box->storage, MAIL_ERROR_PARAMS,
+			"Subscription with matching validation not found");
+		return -1;
+	}
+	/* Finish registration by removing validation from the old stored
+	   subscription. We'll ignore everything else in the new
+	   subscription. */
+	old_subscription.validation = NULL;
+	return webpush_subscription_store(t, device_key, &old_subscription);
+}
+
+static int
+webpush_subscription_attribute_set(struct mailbox_transaction_context *t,
+				   const char *key,
+				   const struct mail_attribute_value *value)
+{
+	struct webpush_subscription subscription;
+	const char *p, *device_key, *storage_key, *error;
+
+	i_assert(str_begins(key, MAILBOX_ATTRIBUTE_WEBPUSH_PUBLIC_SUBSCRIPTION_PREFIX));
+	device_key = key + strlen(MAILBOX_ATTRIBUTE_WEBPUSH_PUBLIC_SUBSCRIPTION_PREFIX);
+	storage_key = t_strconcat(MAILBOX_ATTRIBUTE_WEBPUSH_PRIVATE_SUBSCRIPTION_PREFIX,
+				  device_key, NULL);
+
+	p = strchr(device_key, '/');
+	if (p != NULL) {
+		if (strcmp(p, "/validation") == 0) {
+			device_key = t_strdup_until(device_key, p);
+			return webpush_subscription_validation_set(t, device_key, value);
+		}
+		mail_storage_set_error(t->box->storage, MAIL_ERROR_PARAMS,
+				       "Device key must not contain '/'");
+		return -1;
+	}
+	if (strlen(device_key) > WEBPUSH_DEVICE_KEY_MAX_LENGTH) {
+		mail_storage_set_error(t->box->storage, MAIL_ERROR_PARAMS,
+				       "Device key is too long");
+		return -1;
+	}
+
+	if (value->value == NULL && value->value_stream == NULL) {
+		/* remove the value */
+		if (mailbox_attribute_set(t, MAIL_ATTRIBUTE_TYPE_PRIVATE,
+					  storage_key, value) < 0) {
+			i_error("webpush: Can't remove subscription: %s",
+				mail_storage_get_last_internal_error(t->box->storage, NULL));
+			return -1;
+		}
+		return 0;
+	}
+
+	/* parse the JSON value into a struct */
+	if (webpush_subscription_parse_value(value, pool_datastack_create(),
+					     &subscription, &error) < 0) {
+		mail_storage_set_error(t->box->storage, MAIL_ERROR_PARAMS,
+			t_strdup_printf("Invalid JSON: %s", error));
+		return -1;
+	}
+
+	/* Starting device subscription. Generate a new unique
+	   validation string and send it as a push notification. */
+	unsigned char buf[32];
+	string_t *str = t_str_new(64);
+
+	if (!webpush_subscription_validate(&subscription, &error)) {
+		mail_storage_set_error(t->box->storage, MAIL_ERROR_PARAMS,
+				       t_strdup_printf("Invalid subscription: %s", error));
+		return -1;
+	}
+
+	random_fill(buf, sizeof(buf));
+	base64_encode(buf, sizeof(buf), str);
+	subscription.validation = str_c(str);
+	subscription.create_time = ioloop_time;
+	//FIXME: send push notification
+
+	return webpush_subscription_store(t, device_key, &subscription);
+}
+
+static int
+webpush_subscription_attribute_iter(struct mailbox *box, const char *key_prefix,
+				    pool_t pool, ARRAY_TYPE(const_string) *keys)
+{
+	struct mailbox_attribute_iter *iter;
+	const char *key;
+
+	iter = mailbox_attribute_iter_init(box, MAIL_ATTRIBUTE_TYPE_PRIVATE,
+		MAILBOX_ATTRIBUTE_WEBPUSH_PRIVATE_SUBSCRIPTION_PREFIX);
+	while ((key = mailbox_attribute_iter_next(iter)) != NULL) {
+		if (str_begins(key, key_prefix)) {
+			key = p_strdup(pool, key);
+			array_push_back(keys, &key);
+		}
+	}
+	return mailbox_attribute_iter_deinit(&iter);
+}
+
+static const struct mailbox_attribute_internal
+iattr_webpush_device = {
+	.type = MAIL_ATTRIBUTE_TYPE_PRIVATE,
+	.key = MAILBOX_ATTRIBUTE_WEBPUSH_PUBLIC_SUBSCRIPTION_PREFIX,
+	.rank = MAIL_ATTRIBUTE_INTERNAL_RANK_AUTHORITY,
+	.flags = MAIL_ATTRIBUTE_INTERNAL_FLAG_CHILDREN |
+		MAIL_ATTRIBUTE_INTERNAL_FLAG_VALIDATED,
+
+	.get = webpush_subscription_attribute_get,
+	.set = webpush_subscription_attribute_set,
+	.iter = webpush_subscription_attribute_iter,
+};
+
+void webpush_device_init(void)
+{
+	mailbox_attribute_register_internal(&iattr_webpush_device);
+}

src/webpush/Makefile.am
@@ -9,9 +9,27 @@ dovecot_module_LTLIBRARIES = \
 
 lib05_webpush_plugin_la_SOURCES = \
 	webpush-plugin.c \
+	webpush-subscription.c \
+	webpush-subscription-parser.c \
 	webpush-vapid.c
 
 pkginc_libdir=$(pkgincludedir)
 pkginc_lib_HEADERS = \
 	webpush-plugin.h \
+	webpush-subscription.h \
 	webpush-vapid.h
+
+test_programs = \
+	test-webpush-subscription-parser
+
+noinst_PROGRAMS = $(test_programs)
+
+test_webpush_subscription_parser_SOURCES = test-webpush-subscription-parser.c
+test_webpush_subscription_parser_LDADD = webpush-subscription-parser.lo $(LIBDOVECOT)
+test_webpush_subscription_parser_DEPENDENCIES = webpush-subscription-parser.lo $(LIBDOVECOT_DEPS)
+
+check: check-am check-test
+check-test: all-am
+	for bin in $(test_programs); do \
+	  if ! $(RUN_TEST) ./$$bin; then exit 1; fi; \
+	done

src/webpush/test-webpush-subscription-parser.c
@@ -1,0 +1,158 @@
+/* Copyright (c) 2019 Dovecot authors, see the included COPYING file */
+
+#include "lib.h"
+#include "array.h"
+#include "istream.h"
+#include "webpush-subscription.h"
+#include "test-common.h"
+
+static struct {
+	const char *input;
+	bool success;
+	struct webpush_subscription subscription;
+} tests[] = {
+	/* invalid JSON */
+	{ .input = "\"create_time\": \"2019-08-16T17:59:56Z\"", .success = FALSE },
+	{ .input = "{ \"create_time\": \"2019-08-16T17:59:56Z\"", .success = FALSE },
+	{ .input = "{ \"create_time\": \"2019-08-16T17:59:56Z\" }{}", .success = FALSE },
+
+	/* create_time */
+	{ .input = "{ \"create_time\": \"2019-08-16T17:59:56Z\" }",
+	  .success = TRUE,
+	  .subscription = { .create_time = 1565978396 },
+	},
+	{ .input = "{ \"create_time\": \"2019-08-16T17:59:56\" }", .success = FALSE },
+	{ .input = "{ \"create_time\": \"12345678\" }", .success = FALSE },
+	{ .input = "{ \"create_time\": 12345678 }", .success = FALSE },
+
+	/* validation */
+	{ .input = "{ \"validation\": \"foobar\" }",
+	  .success = TRUE,
+	  .subscription = { .validation = "foobar" },
+	},
+	{ .input = "{ \"validation\": false }", .success = FALSE },
+	{ .input = "{ \"validation\": null }", .success = FALSE },
+
+	/* client_name */
+	{ .input = "{ \"client\": \"testclient\" }",
+	  .success = TRUE,
+	  .subscription = { .client_name = "testclient" },
+	},
+	{ .input = "{ \"client\": false }", .success = FALSE },
+	{ .input = "{ \"client\": null }", .success = FALSE },
+
+	/* device_name */
+	{ .input = "{ \"device\": \"testdevice\" }",
+	  .success = TRUE,
+	  .subscription = { .device_name = "testdevice" },
+	},
+	{ .input = "{ \"device\": false }", .success = FALSE },
+	{ .input = "{ \"device\": null }", .success = FALSE },
+
+	/* msgtype */
+	{ .input = "{ \"msgtype\": \"any\" }",
+	  .success = TRUE,
+	  .subscription = { .msgtype = WEBPUSH_SUBSCRIPTION_MSGTYPE_ANY },
+	},
+	{ .input = "{ \"msgtype\": \"chat\" }",
+	  .success = TRUE,
+	  .subscription = { .msgtype = WEBPUSH_SUBSCRIPTION_MSGTYPE_CHAT },
+	},
+	{ .input = "{ \"msgtype\": \"email\" }",
+	  .success = TRUE,
+	  .subscription = { .msgtype = WEBPUSH_SUBSCRIPTION_MSGTYPE_EMAIL },
+	},
+	{ .input = "{ \"msgtype\": \"\" }", .success = FALSE },
+	{ .input = "{ \"msgtype\": null }", .success = FALSE },
+
+	/* resource endpoint */
+	{ .input = "{ \"resource\": { \"endpoint\": \"http://127.0.0.1/foo\" } }",
+	  .success = TRUE,
+	  .subscription = { .resource_endpoint = "http://127.0.0.1/foo" },
+	},
+	{ .input = "{ \"resource\": { \"endpoint\": null } }", .success = FALSE },
+
+	/* other cases */
+	{ .input = "{ }", .success = TRUE },
+	{ .input = "{ \"foo\": \"bar\" }", .success = TRUE },
+
+	/* resource_keys: keep this the last. The test code writes the
+	   subscription here, since it doesn't seem to be possible otherwise. */
+	{ .input = "{ \"resource\": { \"keys\": { \"p256dh\": \"p256dh-key\", \"auth\": \"auth-key\" } } }",
+	  .success = TRUE,
+	},
+};
+
+static bool
+webpush_subscription_equals(const struct webpush_subscription *s1,
+			    const struct webpush_subscription *s2)
+{
+	const struct webpush_resource_key *s1_keys, *s2_keys;
+	unsigned int i, s1_count, s2_count;
+
+	test_assert(s1->create_time == s2->create_time);
+	test_assert(null_strcmp(s1->client_name, s2->client_name) == 0);
+	test_assert(null_strcmp(s1->device_name, s2->device_name) == 0);
+	test_assert(null_strcmp(s1->validation, s2->validation) == 0);
+	test_assert(s1->msgtype == s2->msgtype);
+	test_assert(null_strcmp(s1->resource_endpoint, s2->resource_endpoint) == 0);
+
+	if (!array_is_created(&s1->resource_keys))
+		test_assert(!array_is_created(&s2->resource_keys));
+	else {
+		s1_keys = array_get(&s1->resource_keys, &s1_count);
+		s2_keys = array_get(&s2->resource_keys, &s2_count);
+		test_assert(s1_count == s2_count);
+		for (i = 0; i < I_MIN(s1_count, s2_count); i++) {
+			test_assert_strcmp(s1_keys[i].key, s2_keys[i].key);
+			test_assert_strcmp(s1_keys[i].value, s2_keys[i].value);
+		}
+	}
+
+	return !test_has_failed();
+}
+
+static void test_webpush_subscription_parser(void)
+{
+	struct webpush_subscription subscription;
+	ARRAY_TYPE(webpush_resource_key) *keys;
+	struct webpush_resource_key *key;
+	struct istream *input;
+	const char *error;
+	pool_t pool;
+	bool success;
+
+	test_begin("webpush subscription parser");
+
+	keys = &tests[N_ELEMENTS(tests)-1].subscription.resource_keys;
+	t_array_init(keys, 2);
+	key = array_append_space(keys);
+	key->key = "p256dh"; key->value = "p256dh-key";
+	key = array_append_space(keys);
+	key->key = "auth"; key->value = "auth-key";
+
+	pool = pool_alloconly_create("webpush subscription test", 1024);
+	for (unsigned int i = 0; i < N_ELEMENTS(tests); i++) {
+		p_clear(pool);
+		input = i_stream_create_from_data(tests[i].input, strlen(tests[i].input));
+		success = webpush_subscription_parse(input, pool, &subscription, &error) == 0;
+		i_stream_unref(&input);
+
+		test_assert(tests[i].success == success);
+		if (success)
+			test_assert(webpush_subscription_equals(&subscription, &tests[i].subscription));
+	}
+
+	pool_unref(&pool);
+	test_end();
+}
+
+int main(void)
+{
+	static void (*const test_functions[])(void) = {
+		test_webpush_subscription_parser,
+		NULL
+	};
+;
+	return test_run(test_functions);
+}

src/webpush/webpush-plugin.c
@@ -9,6 +9,7 @@ const char *webpush_plugin_version = DOVECOT_ABI_VERSION;
 void webpush_plugin_init(struct module *module ATTR_UNUSED)
 {
 	webpush_vapid_init();
+	webpush_device_init();
 }
 
 void webpush_plugin_deinit(void)

src/webpush/webpush-plugin.h
@@ -16,6 +16,8 @@ struct module;
 	MAILBOX_ATTRIBUTE_PREFIX_DOVECOT_PVT_SERVER \
 	MAILBOX_ATTRIBUTE_PREFIX_DOVECOT_PVT"webpush/"
 
+void webpush_device_init(void);
+
 void webpush_plugin_init(struct module *module);
 void webpush_plugin_deinit(void);
 

src/webpush/webpush-subscription-parser.c
@@ -1,0 +1,231 @@
+/* Copyright (c) 2019 Dovecot authors, see the included COPYING file */
+
+#include "lib.h"
+#include "array.h"
+#include "istream.h"
+#include "iso8601-date.h"
+#include "json-parser.h"
+#include "webpush-subscription.h"
+
+#define WEBPUSH_VALUE_MAX_LENGTH 1024
+#define WEBPUSH_MAX_KEYS_COUNT 2
+
+static const char *webpush_subscription_msgtype_strings[] = {
+	[WEBPUSH_SUBSCRIPTION_MSGTYPE_ANY] = "any",
+	[WEBPUSH_SUBSCRIPTION_MSGTYPE_CHAT] = "chat",
+	[WEBPUSH_SUBSCRIPTION_MSGTYPE_EMAIL] = "email",
+};
+
+const char *webpush_subscription_msgtype_to_string(enum webpush_subscription_msgtype msgtype)
+{
+	return webpush_subscription_msgtype_strings[msgtype];
+}
+
+static bool
+webpush_subscription_msgtype_parse(const char *value,
+				   enum webpush_subscription_msgtype *msgtype_r)
+{
+	enum webpush_subscription_msgtype msgtype;
+
+	for (msgtype = WEBPUSH_SUBSCRIPTION_MSGTYPE_ANY;
+	     msgtype < N_ELEMENTS(webpush_subscription_msgtype_strings);
+	     msgtype++) {
+		if (strcmp(webpush_subscription_msgtype_strings[msgtype], value) == 0) {
+			*msgtype_r = msgtype;
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+static bool
+webpush_subscription_get_string(const char *value_in, pool_t pool,
+				const char **value_out_r, const char **error_r)
+{
+	if (strlen(value_in) > WEBPUSH_VALUE_MAX_LENGTH) {
+		*error_r = "Value too long";
+		return FALSE;
+	}
+	*value_out_r = p_strdup(pool, value_in);
+	return TRUE;
+}
+
+static bool
+webpush_subscription_parse_keys(struct json_parser *parser, pool_t pool,
+				struct webpush_subscription *subscription_r,
+				const char **error_r)
+{
+	const char *key, *value;
+	enum json_type type;
+
+	if (json_parse_next(parser, &type, &key) <= 0 ||
+	    type != JSON_TYPE_OBJECT) {
+		*error_r = "keys: Expected object";
+		return FALSE;
+	}
+
+	if (!array_is_created(&subscription_r->resource_keys))
+		p_array_init(&subscription_r->resource_keys, pool, 2);
+	while (json_parse_next(parser, &type, &key) > 0) {
+		if (type == JSON_TYPE_OBJECT_END)
+			return TRUE;
+		if (type != JSON_TYPE_OBJECT_KEY) {
+			*error_r = "keys: Expected object-key";
+			return FALSE;
+		}
+		if (array_count(&subscription_r->resource_keys) >= WEBPUSH_MAX_KEYS_COUNT) {
+			*error_r = "keys: Too many keys";
+			return FALSE;
+		}
+
+		if (!webpush_subscription_get_string(key, pool, &key, error_r))
+			return FALSE;
+
+		if (json_parse_next(parser, &type, &value) <= 0 ||
+		    type != JSON_TYPE_STRING) {
+			*error_r = "keys: Expected string";
+			return FALSE;
+		}
+		struct webpush_resource_key *res_key =
+			array_append_space(&subscription_r->resource_keys);
+		res_key->key = key;
+		if (!webpush_subscription_get_string(value, pool, &res_key->value, error_r))
+			return FALSE;
+	}
+	*error_r = "keys: Expected object-end";
+	return FALSE;
+}
+
+static bool
+webpush_subscription_parse_resource(struct json_parser *parser, pool_t pool,
+				    struct webpush_subscription *subscription_r,
+				    const char **error_r)
+{
+	const char *key, *value;
+	enum json_type type;
+
+	if (json_parse_next(parser, &type, &key) <= 0 ||
+	    type != JSON_TYPE_OBJECT) {
+		*error_r = "resource: Expected object";
+		return FALSE;
+	}
+
+	while (json_parse_next(parser, &type, &key) > 0) {
+		if (type == JSON_TYPE_OBJECT_END)
+			return TRUE;
+		if (type != JSON_TYPE_OBJECT_KEY) {
+			*error_r = "resource: Expected object-key";
+			return FALSE;
+		}
+		if (strcmp(key, "endpoint") == 0) {
+			if (json_parse_next(parser, &type, &value) <= 0 ||
+			    type != JSON_TYPE_STRING) {
+				*error_r = "endpoint: Expected string";
+				return FALSE;
+			}
+			if (!webpush_subscription_get_string(value, pool,
+					&subscription_r->resource_endpoint, error_r))
+				return FALSE;
+		} else if (strcmp(key, "keys") == 0) {
+			if (!webpush_subscription_parse_keys(parser, pool, subscription_r, error_r))
+				return FALSE;
+		} else {
+			json_parse_skip_next(parser);
+		}
+	}
+	*error_r = "resource: Expected object-end";
+	return FALSE;
+}
+
+static bool
+webpush_subscription_parse_root(struct json_parser *parser, pool_t pool,
+				struct webpush_subscription *subscription_r,
+				const char **error_r)
+{
+	const char *key, *value;
+	enum json_type type;
+	int tz;
+
+	while (json_parse_next(parser, &type, &key) > 0) {
+		if (type != JSON_TYPE_OBJECT_KEY) {
+			*error_r = "Expected object-key";
+			return FALSE;
+		}
+
+		const char **value_p = NULL;
+		enum {
+			PARSE_STRP,
+			PARSE_MSGTYPE,
+			PARSE_CREATE_TIME,
+		} parse_type = PARSE_STRP;
+		if (strcmp(key, "client") == 0)
+			value_p = &subscription_r->client_name;
+		else if (strcmp(key, "device") == 0)
+			value_p = &subscription_r->device_name;
+		else if (strcmp(key, "msgtype") == 0)
+			parse_type = PARSE_MSGTYPE;
+		else if (strcmp(key, "validation") == 0)
+			value_p = &subscription_r->validation;
+		else if (strcmp(key, "create_time") == 0)
+			parse_type = PARSE_CREATE_TIME;
+		else if (strcmp(key, "resource") == 0) {
+			if (!webpush_subscription_parse_resource(parser, pool, subscription_r, error_r))
+				return FALSE;
+			continue;
+		} else {
+			/* unknown key */
+			json_parse_skip_next(parser);
+			continue;
+		}
+		if (json_parse_next(parser, &type, &value) <= 0 ||
+		    type != JSON_TYPE_STRING) {
+			*error_r = "Expected string";
+			return FALSE;
+		}
+		switch (parse_type) {
+		case PARSE_STRP:
+			if (!webpush_subscription_get_string(value, pool, value_p, error_r))
+				return FALSE;
+			break;
+		case PARSE_MSGTYPE:
+			if (!webpush_subscription_msgtype_parse(value, &subscription_r->msgtype)) {
+				*error_r = "Unknown msgtype";
+				return FALSE;
+			}
+			break;
+		case PARSE_CREATE_TIME:
+			if (!iso8601_date_parse((const unsigned char *)value,
+						strlen(value),
+						&subscription_r->create_time,
+						&tz)) {
+				*error_r = "create_time: Invalid value";
+				return FALSE;
+			}
+			break;
+		}
+	}
+	return TRUE;
+}
+
+int webpush_subscription_parse(struct istream *input, pool_t pool,
+			       struct webpush_subscription *subscription_r,
+			       const char **error_r)
+{
+	struct json_parser *parser;
+	const char *error;
+
+	i_assert(input->blocking);
+
+	i_zero(subscription_r);
+	parser = json_parser_init(input);
+
+	/* parse root */
+	if (!webpush_subscription_parse_root(parser, pool, subscription_r, error_r)) {
+		(void)json_parser_deinit(&parser, &error);
+		return -1;
+	}
+
+	if (json_parser_deinit(&parser, error_r) < 0)
+		return -1;
+	return 0;
+}

src/webpush/webpush-subscription.h
@@ -1,0 +1,55 @@
+#ifndef WEBPUSH_SUBSCRIPTION_H
+#define WEBPUSH_SUBSCRIPTION_H
+
+struct mailbox;
+
+#include "webpush-plugin.h"
+
+#define MAILBOX_ATTRIBUTE_WEBPUSH_PUBLIC_SUBSCRIPTION_PREFIX \
+	MAILBOX_ATTRIBUTE_WEBPUSH_PUBLIC_PREFIX"subscription/"
+
+#define MAILBOX_ATTRIBUTE_WEBPUSH_PRIVATE_SUBSCRIPTION_PREFIX \
+	MAILBOX_ATTRIBUTE_WEBPUSH_PRIVATE_PREFIX"subscription/"
+
+enum webpush_subscription_msgtype {
+	WEBPUSH_SUBSCRIPTION_MSGTYPE_UNKNOWN = 0,
+	WEBPUSH_SUBSCRIPTION_MSGTYPE_ANY,
+	WEBPUSH_SUBSCRIPTION_MSGTYPE_CHAT,
+	WEBPUSH_SUBSCRIPTION_MSGTYPE_EMAIL,
+};
+
+struct webpush_resource_key {
+	const char *key, *value;
+};
+ARRAY_DEFINE_TYPE(webpush_resource_key, struct webpush_resource_key);
+
+struct webpush_subscription {
+	time_t create_time;
+	const char *validation;
+
+	const char *client_name;
+	const char *device_name;
+	enum webpush_subscription_msgtype msgtype;
+
+	const char *resource_endpoint;
+	ARRAY_TYPE(webpush_resource_key) resource_keys;
+};
+ARRAY_DEFINE_TYPE(webpush_subscription, struct webpush_subscription);
+
+/* Lookup a subscription from mailbox attributes and parse it. The device_key
+   contains only the unique device ID, i.e. it doesn't include the
+   MAILBOX_ATTRIBUTE_WEBPUSH_PUBLIC_SUBSCRIPTION_PREFIX.
+
+   Returns 1 if ok, 0 if device_key doesn't exist, -1 on internal error.
+   Corrupted subscriptions return 0 and they are immediately deleted. */
+int webpush_subscription_read(struct mailbox *box, const char *device_key,
+			      pool_t pool,
+			      struct webpush_subscription *subscription_r);
+
+int webpush_subscription_parse(struct istream *input, pool_t pool,
+			       struct webpush_subscription *subscription_r,
+			       const char **error_r);
+
+const char *webpush_subscription_msgtype_to_string(enum webpush_subscription_msgtype msgtype);
+
+#endif

commit 30e88e7cd084127bfe81f92a8e0780eb13df6706

webpush: Fix typo in validate
Spec says that the suffix is /validate, not /validation.

Broken in 39da2df1a3d8d859e7673f0da258ff1b6c8d5e95

src/webpush/webpush-subscription.c
@@ -452,7 +452,7 @@ webpush_subscription_attribute_set(struct mailbox_transaction_context *t,
 
 	p = strchr(device_key, '/');
 	if (p != NULL) {
-		if (strcmp(p, "/validation") == 0) {
+		if (strcmp(p, "/validate") == 0) {
 			device_key = t_strdup_until(device_key, p);
 			return webpush_subscription_validation_set(t, device_key, value);
 		}